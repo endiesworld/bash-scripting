9d57d7bfe8b550680b227d66a1072d1440954984
endiesworld <endiesworld@gmail.com>
2023-03-26 05:20:58 +0100

    feat: Added comments for compare two dir script

--- compare-two-dir.sh
+++ compare-two-dir.sh
@@ -25,3 +25,17 @@ find . -type f -exec test ! -e ../dir2/{} \; -a -exec echo "{} is present in $di
 
 cd ../dir2
 find . -type f -exec test ! -e ../dir1/{} \; -a -exec echo "{} is present in $dir2 but not in $dir1" \;
+
+# This script performs a search for files inside the directory dir1 and its subdirectories. 
+# Here is a breakdown of each component of the script:
+
+# cd dir1: This changes the current working directory to dir1.
+# This is done so that the find command only searches for files within dir1 and its subdirectories.
+# find . -type f: This command uses the find utility to search for files in the current directory (.) and its subdirectories. 
+# The -type f option restricts the search to only regular files.
+# -exec test ! -e ../dir2/{} \;: This is a test that checks if a file exists in ../dir2. 
+# The {} is a placeholder for the current file being examined. The ! -e option checks if the file does not exist.
+# -a: This option specifies that the next command should only be executed if the previous command (the test) returns true.
+# -exec echo "{}": This command echoes the file name of each file that passes the previous test.
+# To summarize, the script searches for regular files in dir1 and its subdirectories that do not exist in the dir2 directory. 
+# It then prints the name of each file that passes this test.
